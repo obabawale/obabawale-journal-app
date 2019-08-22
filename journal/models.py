8d5811bdd64d2ebb0702593bee0eea76ba5621dc
obabawale <padinality@yahoo.com>
2019-08-22 10:25:03 +0100

    Add my app to the admin site and set url patterns

--- journal/models.py
+++ journal/models.py
@@ -2,5 +2,5 @@ from django.db import models
 
 # Create your models here.
 class Journal(models.Model):
-    title = models.CharField(max_length=3)
-    body = models.CharField(max_length=64)
+    title = models.CharField(max_length=64)
+    body = models.TextField()
